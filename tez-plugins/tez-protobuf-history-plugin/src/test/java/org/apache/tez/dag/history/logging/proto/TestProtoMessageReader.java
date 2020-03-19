76c18ce3646989054d3220c354fb3c07a5d99000
Laszlo Bodor <bodorlaszlo0202@gmail.com>
2020-03-19 23:53:18 +0100

    temp: proto tester

---
+++ tez-plugins/tez-protobuf-history-plugin/src/test/java/org/apache/tez/dag/history/logging/proto/TestProtoMessageReader.java
@@ -0,0 +1,40 @@
+package org.apache.tez.dag.history.logging.proto;
+
+import java.io.File;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.hadoop.fs.Path;
+import org.apache.tez.dag.api.TezConfiguration;
+import org.apache.tez.dag.history.logging.proto.HistoryLoggerProtos.HistoryEventProto;
+import org.codehaus.jettison.json.JSONObject;
+import org.junit.Test;
+
+public class TestProtoMessageReader {
+
+  @Test
+  public void testReader() throws Exception {
+    final HistoryEventProtoConverter converter = new HistoryEventProtoConverter();
+    ProtoMessageReader r = new ProtoMessageReader(new TezConfiguration(),
+        new Path("src/test/resources/dag_1583980529217_0000_18_1_1"), HistoryEventProto.PARSER);
+
+    HistoryEventProto message = (HistoryEventProto)r.readEvent();
+    int index = 0;
+    while (message != null){
+      index+=1;
+      //String eventType = message.getEventType();
+      //File folder = new File("src/test/resources/" + eventType);
+      //folder.mkdirs();
+
+      JSONObject jsonObject = HistoryEventProtoJsonConversion.convertToJson(message);
+      System.out.println("*****" + jsonObject);
+
+      //FileUtils.writeStringToFile(new File(folder, index + ".txt"), message.toString());
+      try {
+        message = (HistoryEventProto) r.readEvent();
+      } catch (java.io.EOFException e) {
+        message = null;
+      }
+    }
+    System.out.println("*****" + index);
+  }
+}
