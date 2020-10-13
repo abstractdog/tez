00110249bfde9f934a747d0580d28e32ecc2e8f5
Laszlo Bodor <bodorlaszlo0202@gmail.com>
2020-10-13 13:55:37 +0200

    TEZ-4236: Investigate if DAGAppMaster's DAGClientServer is really need to be started/used in local mode

--- tez-dag/src/main/java/org/apache/tez/dag/app/DAGAppMaster.java
+++ tez-dag/src/main/java/org/apache/tez/dag/app/DAGAppMaster.java
@@ -509,8 +509,7 @@ public class DAGAppMaster extends AbstractService {
     recoveryEnabled = conf.getBoolean(TezConfiguration.DAG_RECOVERY_ENABLED,
         TezConfiguration.DAG_RECOVERY_ENABLED_DEFAULT);
 
-    clientRpcServer = new DAGClientServer(clientHandler, appAttemptID, recoveryFS);
-    addIfService(clientRpcServer, true);
+    initClientRpcServer();
 
     taskHeartbeatHandler = createTaskHeartbeatHandler(context, conf);
     addIfService(taskHeartbeatHandler, true);
@@ -646,6 +645,11 @@ public class DAGAppMaster extends AbstractService {
     } else {
       this.state = DAGAppMasterState.ERROR;
     }
+  }
+
+  protected void initClientRpcServer() {
+    clientRpcServer = new DAGClientServer(clientHandler, appAttemptID, recoveryFS);
+    addIfService(clientRpcServer, true);
   }
 
   @VisibleForTesting
