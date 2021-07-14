16189b385ef81bc1e15b96a39d0172e197907307
Laszlo Bodor <bodorlaszlo0202@gmail.com>
2021-07-14 23:26:57 +0200

    first commit, not very proud

---
+++ tez-tools/tez-tools-webapp/src/main/java/com/baeldung/thymeleaf/config/InitSecurity.java
@@ -0,0 +1,10 @@
+package com.baeldung.thymeleaf.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class InitSecurity extends AbstractSecurityWebApplicationInitializer {
+
+    public InitSecurity() {
+        super(WebMVCSecurity.class);
+    }
+}
